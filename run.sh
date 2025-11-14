feb51fdfbf66c6e1e96f7c923995ee97d4b27787
Samuel Lampa <samuel.lampa@scilifelab.se>
2025-11-14 16:19:01 +0100

    Move work dir under data dir

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-inst_dir=$(pwd)
+installdir=$(pwd)
+nfdir=/home/grid/opt/trana
+workdir=/data/trana/work
 start_timestamp=$(date +%Y%m%d-%H%M%S)
-outdir=/data/trana-output/${start_timestamp}
-cd /home/grid/opt/trana && \
+outdir=/data/trana/results/trana_run_${start_timestamp}
+cd ${nfdir} && \
 pixi run nextflow  \
-    -c ${inst_dir}/gridion.config \
+    -c ${installdir}/gridion.config \
     -log $(pwd)/nextflow.log \
     run main.nf \
     -profile docker,gridion \
     --outdir ${outdir} \
     --db $(pwd)/assets/databases/emu_database \
-    --input https://raw.githubusercontent.com/genomic-medicine-sweden/test-datasets/refs/heads/16s/samplesheet.csv
+    --krona_taxonomy_tab $(pwd)/assets/databases/krona/taxonomy/taxonomy.tab \
+    --input https://raw.githubusercontent.com/genomic-medicine-sweden/test-datasets/refs/heads/16s/samplesheet.csv \
+    -w ${workdir}
