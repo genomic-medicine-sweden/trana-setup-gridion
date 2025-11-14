c0f70ef9b0be744dcb35436f8cfb0d91912ff7b1
Samuel Lampa <samuel.lampa@scilifelab.se>
2025-11-14 15:31:47 +0100

    Update config and run script

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-nextflow  \
+inst_dir=$(pwd)
+start_timestamp=$(date +%Y%m%d-%H%M%S)
+outdir=/data/trana-output/${start_timestamp}
+cd /home/grid/opt/trana && \
+pixi run nextflow  \
+    -c ${inst_dir}/gridion.config \
     -log $(pwd)/nextflow.log \
     run main.nf \
-    -profile docker,test \
-    --outdir results \
+    -profile docker,gridion \
+    --outdir ${outdir} \
     --db $(pwd)/assets/databases/emu_database \
     --input https://raw.githubusercontent.com/genomic-medicine-sweden/test-datasets/refs/heads/16s/samplesheet.csv
